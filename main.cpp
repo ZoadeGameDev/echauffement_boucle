3ca8544a26fc53dad3a3d9b10fa5dc730c590a83
Zoade <zoade.edaoz@gmail.com>
2026-09-23 12:02:47 +0200

    Exercice 3 resolved

--- main.cpp
+++ main.cpp
@@ -10,19 +10,31 @@ int main() {
     }
 
     // Exercice 1.2 -----
-    int munitions = 7;
-    do {
-        std::println("munitions : {}",munitions);
-        int evenOrOdd = munitions % 2;
-        if (evenOrOdd == 0) {
-            std::println("pair number of munitions");
-        } else {
-            std::println("odd number of munitions");
-        }
+    // int munitions = 7;
+    // do {
+    //     std::println("munitions : {}",munitions);
+    //     int evenOrOdd = munitions % 2;
+    //     if (evenOrOdd == 0) {
+    //         std::println("pair number of munitions");
+    //     } else {
+    //         std::println("odd number of munitions");
+    //     }
+    //
+    //     munitions = munitions - 1;
+    //
+    // } while (munitions > 0);
 
-        munitions = munitions - 1;
-
-    } while (munitions > 0);
+    // Exercice 1.3 -----
+    int character_hp = 0;
+    if (character_hp <= 0) {
+        std::println("Mort");
+    } else if (character_hp <= 40 ){
+        std::println("Critique");
+    } else if (character_hp <= 75 ) {
+        std::println("Egratigne");
+    } else {
+        std::println("Intact");
+    }
 
     return 0;
 }
