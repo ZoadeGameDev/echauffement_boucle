5dbd9b21eccab05267eb0374ae2e02be8b3fc5da
Zoade <zoade.edaoz@gmail.com>
2026-09-23 11:45:20 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <print>
+
+int main() {
+
+    // Exercice 1.1 ----
+    int pv = 42;
+    if (pv <= 0) {
+        std::println("Game Over");
+    }
+
+    return 0;
+}
